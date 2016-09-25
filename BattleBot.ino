d8ee2400f5570e994a3fbb94f4ca9231f76161a0
Neil Dhar <pranks16@gmail.com>
2016-09-25 05:03:23 -0400

    added direction function

--- BattleBot.ino
+++ BattleBot.ino
@@ -1,8 +1,28 @@
 #include <Wire.h>
 #include "HMC5883L.h"
-//hi
 
 HMC5883L compass;
+
+int XPos, YPos;
+
+int getDir(int x, int y){
+    int dx = x-xPos, dy = y-YPos;
+    if(dy==0&&dx==0)
+        return -1;
+    else if(dy==0)
+        return dx>0?90:-90;
+    else if(dx==0)
+        return dy>0?0:180;
+    int basic = abs(atan2(dy,dx));
+    if(dy>0 && dx >0)
+        return 90-basic;
+    else if(dx<0 && dy>0)
+        return basic-90;
+    else if(dx>0 && dy<0)
+        return basic+90;
+    else if(dx<0 && dy<0)
+        return -90-basic;
+}
 void setup(){
     Serial.begin(9600);
     while (!compass.begin()){
@@ -12,8 +32,12 @@ void setup(){
 }
 
 void loop(){
+    //READ DATA FROM RECEIVER HERE
+    //CHECK FOR RED LINES AND USE RECEIVER DATA
+    //SET COURSE
     Vector data  = compass.readNormalize();
     double bearing  = (data.YAxis!=0) ? atan2(data.XAxis, data.YAxis)/0.0174532925 : (data.XAxis>0 ? 0.0 : 180.0);
     Serial.println(bearing);
+    Serial.println(data.YAxis);
 }
 
